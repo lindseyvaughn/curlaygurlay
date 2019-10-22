9e552cd2a68bf8f7e95825dc1294b61869d9ac42
laurenseyranian <lauren.seyranian@gmail.com>
2019-10-22 16:38:51 -0700

    Update

--- apps/first_app/urls.py
+++ apps/first_app/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^curlaygurlay$', views.home_page),
-    url(r'^curlaygurlay/loose_curl$', views.loose_curl),
-    url(r'^curlaygurlay/tight_curl$', views.tight_curl),
-    url(r'^curlaygurlay/kinky_curl$', views.kinky_curl),
+    url(r'^curlaygurlay/loose_curl$', views.loose_curl_page),
+    url(r'^curlaygurlay/tight_curl$', views.tight_curl_page),
+    url(r'^curlaygurlay/kinky_curl$', views.kinky_curl_page),
+
+    url(r'^curlaygurlay/about$', views.about_page),
+    url(r'^curlaygurlay/products$', views.products_page),
+    url(r'^curlaygurlay/blog$', views.blog_page),
 ]
--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -4,11 +4,21 @@ from django.shortcuts import render, HttpResponse
 def home_page(request):
     return render(request, 'first_app/home_page.html')
 
-def loose_curl(request):
-    return render(request, 'first_app/loose_curl.html')
+def loose_curl_page(request):
+    return render(request, 'first_app/loose_curl_page.html')
 
-def kinky_curl(request):
-    return render(request, 'first_app/kinky_curl.html')
+def kinky_curl_page(request):
+    return render(request, 'first_app/kinky_curl_page.html')
 
-def tight_curl(request):
-    return render(request, 'first_app/tight_curl.html')
+def tight_curl_page(request):
+    return render(request, 'first_app/tight_curl_page.html')
+
+def about_page(request):
+    return render(request, 'first_app/about_page.html')
+
+def products_page(request):
+    return render(request, 'first_app/products_page.html')
+
+def blog_page(request):
+    return render(request, 'first_app/blog_page.html')
+
